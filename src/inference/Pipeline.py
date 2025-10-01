a94e86e76c9eb7a98f72ad62dfc9ce9bfcefc758
Jithsungh Sai Vallabhaneni (TM) <jithsungh.v@techmojo.in>
2025-10-01 15:08:23 +0530

    refactor: update LLM client import to use LLMClientGemma

--- src/inference/Pipeline.py
+++ src/inference/Pipeline.py
@@ -32,7 +32,7 @@ class Pipeline:
             from src.inference.HybridRouter import HybridDepartmentRouter
             from src.inference.ContextRetriever import SimpleContextRetriever
             from src.inference.PromptGenerator import PromptGenerator
-            from src.utils.LLMClientOpenAI import LLMClientOpenAI as LLMClient
+            from src.utils.LLMClientGemma import LLMClientGemma as LLMClient
             from src.utils.ResponseFormatter import ResponseFormatter
             from src.config import Config
             
